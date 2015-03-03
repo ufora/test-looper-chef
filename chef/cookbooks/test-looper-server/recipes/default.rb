ae3c47aa42ac6841146ce95f8fb3943979f17642
Ronen Hilewicz <ronen@ufora.com>
2015-03-03 02:13:35 -0500

    test-looper-server.conf in cookbook

--- chef/cookbooks/test-looper-server/recipes/default.rb
+++ chef/cookbooks/test-looper-server/recipes/default.rb
@@ -6,7 +6,8 @@
 
 credentials = Chef::EncryptedDataBagItem.load('test_looper', 'server')
 install_dir = node[:test_looper_server][:install_dir]
-deploy_key = node[:test_looper_server][:deploy_key]
+git_branch = node[:test_looper_server][:git_branch]
+deploy_key = node[:test_looper_server][:github_deploy_key]
 service_account = node[:test_looper_server][:service_account]
 
 apt_package 'git' do
@@ -44,11 +45,26 @@ file "#{install_dir}/#{deploy_key}" do
   mode '0700'
 end
 
+template "/etc/init/test-looper-server.conf" do
+  source "test-looper-server-upstart-conf.erb"
+  variables({
+      :service_account => service_account,
+      :install_dir => install_dir,
+      :src_dir => "#{install_dir}/src",
+      :git_branch => git_branch,
+      :github_login => node[:test_looper_server][:github_login],
+      :github_token => credentials['github_token'],
+      :test_looper_github_auth_secret => credentials['test_looper_github_auth_secret'],
+      :test_looper_github_app_client_secret => credentials['test_looper_github_app_client_secret']
+  })
+end
+
 deploy_revision "#{install_dir}/src" do
   repo node[:test_looper_server][:git_repo]
-  revision node[:test_looper_server][:git_branch]
+  revision git_branch
   ssh_wrapper "#{install_dir}/wrap-ssh4git.sh"
   user service_account
+  group service_account
   action :deploy
 
   symlink_before_migrate.clear
