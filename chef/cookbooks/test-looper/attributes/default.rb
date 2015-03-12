22a2efc954153a9f805675878886956aad53e3b3
Tom Peters <thomas.d.peters@gmail.com>
2015-03-12 18:06:27 -0400

    adding some setup for cloning repo

--- chef/cookbooks/test-looper/attributes/default.rb
+++ chef/cookbooks/test-looper/attributes/default.rb
@@ -1,1 +1,4 @@
 default["test_looper"]["tester_account"] = "tester0"
+default["test_looper"]["github_deploy_key"] = "id_deploy"
+default["test_looper"]["install_dir"] = "/opt/test-looper"
+
--- chef/cookbooks/test-looper/recipes/default.rb
+++ chef/cookbooks/test-looper/recipes/default.rb
@@ -7,9 +7,16 @@
 include_recipe 'test-looper::apt-packages'
 include_recipe 'test-looper::python-modules'
 
-tester_account = node[:test_looper][:tester_account]
+service_account = node[:test_looper][:service_account]
+install_dir = node[:test_looper][:install_dir]
 
-user tester_account do
+ssh_dir = "#{install_dir}/.ssh"
+deploy_key = "#{ssh_dir}/#{node[:test_looper][:github_deploy_key]}"
+
+secrets = Chef::EncryptedDataBagItem.load('test-looper', 'server')
+git_deploy_key = secrets['git_deploy_key']
+
+user service_account do
   action :create
   shell '/bin/bash'
 end
@@ -21,10 +28,28 @@ end
 group "docker" do
   action :modify
   append true
-  members tester_account
+  members service_account
 end
 
 service "docker.io" do
   action :restart
 end
 
+# Create the git ssh wrapper that uses our deployment key
+template git_ssh_wrapper do
+  source 'git-ssh-wrapper.erb'
+  owner service_account
+  group service_account
+  mode '0700'
+  variables({
+      :deploy_key => deploy_key
+  })
+end
+
+# Create the git ssh key (deployment key)
+file deploy_key do
+  content git_deploy_key
+  owner service_account
+  group service_account
+  mode '0700'
+end
