da78ed925080c3960ce47b949a1107ed08bcd9ca
Tom Peters <thomas.d.peters@gmail.com>
2015-03-12 16:18:50 -0400

    setting up initial cookbook for test-looper

---
+++ chef/cookbooks/test-looper/README.md
@@ -0,0 +1,4 @@
+# test-looper
+
+TODO: Enter the cookbook description here.
+
---
+++ chef/cookbooks/test-looper/metadata.rb
@@ -0,0 +1,9 @@
+name             'test-looper'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures test-looper'
+long_description 'Installs/Configures test-looper'
+version          '0.1.0'
+
+depends "apt", "= 2.6.1"
