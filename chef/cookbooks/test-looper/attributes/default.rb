95129fedb0f01267ae60922fc26564e4ac44ac99
Tom Peters <thomas.d.peters@gmail.com>
2015-03-13 10:01:48 -0400

    working cookbook for test-looper?

--- chef/cookbooks/test-looper/attributes/default.rb
+++ chef/cookbooks/test-looper/attributes/default.rb
@@ -1,4 +1,8 @@
-default["test_looper"]["tester_account"] = "tester0"
+default["test_looper"]["service_account"] = "tester0"
 default["test_looper"]["github_deploy_key"] = "id_deploy"
 default["test_looper"]["install_dir"] = "/opt/test-looper"
 
+default["test_looper"]["git_ssh_wrapper"] = "wrap-ssh4git.sh"
+
+default["test_looper"]["git_repo"] = "git@github.com:ufora/main.git"
+default["test_looper"]["git_branch"] = "test-looper"
--- chef/cookbooks/test-looper/recipes/default.rb
+++ chef/cookbooks/test-looper/recipes/default.rb
@@ -10,8 +10,15 @@ include_recipe 'test-looper::python-modules'
 service_account = node[:test_looper][:service_account]
 install_dir = node[:test_looper][:install_dir]
 
+src_dir = "#{install_dir}/src"
 ssh_dir = "#{install_dir}/.ssh"
 deploy_key = "#{ssh_dir}/#{node[:test_looper][:github_deploy_key]}"
+git_ssh_wrapper = "#{ssh_dir}/#{node[:test_looper][:git_ssh_wrapper]}"
+
+git_branch = node[:test_looper][:git_branch]
+
+log_file = "/var/log/test-looper.log"
+stack_file = "#{log_file}.stack"
 
 secrets = Chef::EncryptedDataBagItem.load('test-looper', 'server')
 git_deploy_key = secrets['git_deploy_key']
@@ -19,6 +26,15 @@ git_deploy_key = secrets['git_deploy_key']
 user service_account do
   action :create
   shell '/bin/bash'
+end
+
+# Create installation and supporting directories
+[install_dir, ssh_dir].each do |path|
+  directory path do
+    owner service_account
+    group service_account
+    action :create
+  end
 end
 
 group "docker" do
@@ -35,6 +51,22 @@ service "docker.io" do
   action :restart
 end
 
+# Create the git ssh key (deployment key)
+file deploy_key do
+  content git_deploy_key
+  owner service_account
+  group service_account
+  mode '0700'
+end
+
+# Create log files and set their ownership
+[log_file, stack_file].each do |logfile|
+  file logfile do
+    owner service_account
+    group service_account
+  end
+end
+
 # Create the git ssh wrapper that uses our deployment key
 template git_ssh_wrapper do
   source 'git-ssh-wrapper.erb'
@@ -46,10 +78,17 @@ template git_ssh_wrapper do
   })
 end
 
-# Create the git ssh key (deployment key)
-file deploy_key do
-  content git_deploy_key
-  owner service_account
+# Clone the repo into the installation directory
+deploy_revision src_dir do
+  repo node[:test_looper][:git_repo]
+  revision git_branch
+  ssh_wrapper git_ssh_wrapper
+  user service_account
   group service_account
-  mode '0700'
+  action :deploy
+
+  symlink_before_migrate.clear
+  create_dirs_before_symlink.clear
+  purge_before_symlink.clear
+  symlinks.clear
 end
