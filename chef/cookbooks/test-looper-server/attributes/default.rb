ed18accba797d66caf826dcbf8c0d0637a9737aa
Tom Peters <thomas.d.peters@gmail.com>
2015-03-17 10:16:57 -0400

    track test-looper-2 in attributes

--- chef/cookbooks/test-looper-server/attributes/default.rb
+++ chef/cookbooks/test-looper-server/attributes/default.rb
@@ -9,7 +9,7 @@ default["test_looper_server"]["http_port"] = "8888"
 default["test_looper_server"]["git_ssh_wrapper"] = "wrap-ssh4git.sh"
 
 default["test_looper_server"]["git_repo"] = "git@github.com:ufora/main.git"
-default["test_looper_server"]["git_branch"] = "test-looper"
+default["test_looper_server"]["git_branch"] = "test-looper-2"
 default["test_looper_server"]["github_deploy_key"] = "id_deploy"
 default["test_looper_server"]["github_login"] = "ufora-bot"
 default["test_looper_server"]["github_oauth_app_id"] = "eb57b8cf7f9122dad732"
--- chef/cookbooks/test-looper/attributes/default.rb
+++ chef/cookbooks/test-looper/attributes/default.rb
@@ -5,7 +5,7 @@ default["test_looper"]["test_looper_install_dir"] = "/opt/test-looper"
 default["test_looper"]["git_ssh_wrapper"] = "wrap-ssh4git.sh"
 
 default["test_looper"]["git_repo"] = "git@github.com:ufora/main.git"
-default["test_looper"]["test_looper_git_branch"] = "test-looper"
+default["test_looper"]["test_looper_git_branch"] = "test-looper-2"
 default["test_looper"]["github_login"] = "ufora-bot"
 
 default['apt']['compile_time_update'] = true
