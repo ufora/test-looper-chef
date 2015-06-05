d140740e20a02f61f44e2e58b5040ce71650a994
Ronen Hilewicz <ronen@ufora.com>
2015-06-05 15:11:54 -0400

    Copying deploy key and certs

--- test-looper-server/recipes/setup.rb
+++ test-looper-server/recipes/setup.rb
@@ -13,20 +13,16 @@ home_dir = "/home/#{service_account}"
 
 install_dir = node[:test_looper_server][:install_dir]
 ssh_dir = "#{install_dir}/.ssh"
+deploy_key = "#{ssh_dir}/#{node[:test_looper_server][:github_deploy_key]}"
+
 tasks_root_dir = "#{install_dir}/tasks"
 deploy_dir = "#{install_dir}/deploy-src"
 
 #config_file = "#{install_dir}/test-looper-server.conf"
 #src_dir = "#{install_dir}/src"
 #service_dir = "#{src_dir}/current"
-#deploy_key = "#{ssh_dir}/#{node[:test_looper_server][:github_deploy_key]}"
 #git_ssh_wrapper = "#{ssh_dir}/#{node[:test_looper_server][:git_ssh_wrapper]}"
 
-#ssl_dir = node[:test_looper_server][:ssl_dir]
-#cert_name = node[:test_looper_server][:ssl_cert_prefix]
-#public_cert_file = "#{ssl_dir}/#{cert_name}.crt"
-#private_key_file = "#{ssl_dir}/#{cert_name}.key"
-#cert_chain_file = "#{ssl_dir}/#{cert_name}.ca"
 
 require 'aws-sdk'
 s3 = AWS::S3.new
@@ -57,6 +53,37 @@ end
 # Create the git ssh key (deployment key)
 file deploy_key do
   content secrets['git_deploy_key']
+  owner service_account
+  group service_account
+  mode '0700'
+end
+
+
+# Copy SSL Certificate
+ssl_dir = node[:test_looper_server][:ssl_dir]
+cert_name = node[:test_looper_server][:ssl_cert_prefix]
+public_cert_file = "#{ssl_dir}/#{cert_name}.crt"
+private_key_file = "#{ssl_dir}/#{cert_name}.key"
+cert_chain_file = "#{ssl_dir}/#{cert_name}.ca"
+
+ssl_private_key = secrets['ssl_private_key']
+ssl_public_cert = secrets['ssl_public_cert']
+ssl_chain = secrets['ssl_chain']
+
+file public_cert_file do
+  content ssl_public_cert
+  owner service_account
+  group service_account
+  mode '0700'
+end
+file private_key_file do
+  content ssl_private_key
+  owner service_account
+  group service_account
+  mode '0700'
+end
+file cert_chain_file do
+  content ssl_chain
   owner service_account
   group service_account
   mode '0700'
