79df3b1acf6b81508af8111a3511103544cec309
Ronen Hilewicz <ronen@ufora.com>
2015-06-08 12:02:26 -0400

    Splitting server recipe to externals.rb and setup.rb

--- test-looper-server/recipes/setup.rb
+++ test-looper-server/recipes/setup.rb
@@ -18,10 +18,13 @@ deploy_key = "#{ssh_dir}/#{node[:test_looper_server][:github_deploy_key]}"
 tasks_root_dir = "#{install_dir}/tasks"
 deploy_dir = "#{install_dir}/deploy-src"
 
-#config_file = "#{install_dir}/test-looper-server.conf"
-#src_dir = "#{install_dir}/src"
-#service_dir = "#{src_dir}/current"
-#git_ssh_wrapper = "#{ssh_dir}/#{node[:test_looper_server][:git_ssh_wrapper]}"
+config_file = "#{install_dir}/test-looper-server.conf"
+src_dir = "#{install_dir}/src"
+service_dir = "#{src_dir}/current"
+git_ssh_wrapper = "#{ssh_dir}/#{node[:test_looper_server][:git_ssh_wrapper]}"
+
+log_file = "/var/log/test-looper-server.log"
+stack_file = "#{log_file}.stack"
 
 
 require 'aws-sdk'
@@ -89,4 +92,110 @@ file cert_chain_file do
   owner service_account
   group service_account
   mode '0700'
+end
+
+# Create the git ssh wrapper that uses our deployment key
+template git_ssh_wrapper do
+  source 'git-ssh-wrapper.erb'
+  owner service_account
+  group service_account
+  mode '0700'
+  variables({
+      :deploy_key => deploy_key
+  })
+end
+
+# Create log files and set their ownership
+[log_file, stack_file].each do |logfile|
+  file logfile do
+    owner service_account
+    group service_account
+  end
+end
+
+# Clone the repo into the installation directory
+git_branch = node[:test_looper_server][:git_branch]
+deploy_revision src_dir do
+  repo node[:test_looper_server][:git_repo]
+  revision git_branch
+  ssh_wrapper git_ssh_wrapper
+  user service_account
+  group service_account
+  action :deploy
+
+  symlink_before_migrate.clear
+  create_dirs_before_symlink.clear
+  purge_before_symlink.clear
+  symlinks.clear
+end
+
+
+http_port = node[:test_looper_server][:http_port]
+ec2_security_group = node[:test_looper_server][:ec2_worker_security_group]
+ec2_looper_ami = node[:test_looper_server][:ec2_worker_ami]
+ec2_worker_role_name = node[:test_looper_server][:ec2_worker_role_name]
+ec2_worker_ssh_key_name = node[:test_looper_server][:ec2_worker_ssh_key_name]
+ec2_worker_root_volume_size_gb = node[:test_looper_server][:ec2_worker_root_volume_size_gb]
+ec2_test_result_bucket = node[:test_looper][:ec2_test_result_bucket]
+worker_config_file = "#{node[:test_looper][:install_dir]}/#{node[:test_looper][:config_file]}"
+
+template config_file do
+  source "test-looper-server.conf.erb"
+  owner service_account
+  group service_account
+  variables({
+    :server_port => node[:test_looper_server][:port],
+    :server_http_port => http_port,
+    :github_app_id => node[:test_looper_server][:github_oauth_app_id],
+    :github_app_secret => secrets['test_looper_github_app_client_secret'],
+    :github_access_token => secrets['github_api_token'],
+    :github_webhook_secret => secrets['test_looper_github_webhook_secret'],
+    :github_test_looper_branch => git_branch,
+    :ec2_security_group => ec2_security_group,
+    :ec2_ami => ec2_looper_ami,
+    :ec2_worker_role_name => ec2_worker_role_name,
+    :ec2_worker_ssh_key_name => ec2_worker_ssh_key_name,
+    :ec2_worker_root_volume_size_gb => ec2_worker_root_volume_size_gb,
+    :ec2_test_result_bucket => ec2_test_result_bucket,
+    :worker_config_file => worker_config_file
+    })
+end
+
+template "/etc/init/test-looper-server.conf" do
+  source "test-looper-server-upstart-conf.erb"
+  variables({
+      :service_account => service_account,
+      :service_dir => service_dir,
+      :git_ssh_wrapper => git_ssh_wrapper,
+      :log_file => log_file,
+      :stack_file => stack_file,
+      :git_branch => git_branch,
+      :tasks_root => tasks_root_dir,
+      :deploy_dir => deploy_dir,
+      :dependencies_version => node[:test_looper_server][:expected_dependencies_version],
+      :config_file => config_file
+  })
+end
+
+
+web_app "test-looper-proxy" do
+  template "web_app.conf.erb"
+  server_name node[:test_looper_server][:dnsname]
+  cert_file public_cert_file
+  cert_key private_key_file
+  cert_chain cert_chain_file
+  http_port http_port
+end
+
+apache_site "test-looper-proxy" do
+  enable true
+end
+
+service "test-looper-server" do
+  action [:stop, :start]
+  provider Chef::Provider::Service::Upstart
+end
+
+service "apache2" do
+  action :restart
 end
