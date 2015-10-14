50e55710fa91c539358eb173071a07fd7732a5b8
Ronen Hilewicz <ronen@ufora.com>
2015-10-14 11:00:41 -0400

    Adding btrfs-tools to worker image

--- test-looper/recipes/worker-apt.rb
+++ test-looper/recipes/worker-apt.rb
@@ -7,3 +7,11 @@ end
 apt_package 'docker.io' do
   action :install
 end
+
+# We configure docker to keep its data dir on the instance-store
+# and that causes problems with the default filesystem (ext3).
+# See: https://github.com/docker/docker/issues/4036
+# We reformat the instance store using btrfs to work around the issue.
+apt_package 'btrfs-tools' do
+  action :install
+end
