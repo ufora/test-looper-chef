21672137afc42a4a356805647dd308c6e4da5bce
Ronen Hilewicz <ronen@ufora.com>
2015-11-06 15:38:51 -0500

    Getting latest version of docker

--- test-looper/recipes/docker.rb
+++ test-looper/recipes/docker.rb
@@ -1,6 +1,19 @@
 # assumes that the docker apt package has been installed
 # see worker-apt.rb for an example
 
+apt_repository 'docker' do
+  uri 'https://apt.dockerproject.org/repo'
+  components ['main']
+  distribution 'ubuntu-trusty'
+  key '58118E89F3A912897C070ADBF76221572C52609D'
+  keyserver 'hkp://pgp.mit.edu:80'
+  action :add
+end
+
+apt_package 'docker-engine' do
+  action :install
+end
+
 service_account = node[:test_looper][:service_account]
 graph_dir = node[:test_looper][:docker][:graph_dir]
 tmp_dir = node[:test_looper][:docker][:tmp_dir]
--- test-looper/recipes/worker-apt.rb
+++ test-looper/recipes/worker-apt.rb
@@ -4,10 +4,9 @@ end
 apt_package 'python-pip' do
   action :install
 end
-apt_package 'docker.io' do
+apt_package 'apt-transport-https' do
   action :install
 end
-
 # We configure docker to keep its data dir on the instance-store
 # and that causes problems with the default filesystem (ext3).
 # See: https://github.com/docker/docker/issues/4036
